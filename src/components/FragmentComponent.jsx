8103b39dc82ae5c0cd661fc10f41edb9724a0c46
peninah98 <peninangizwenayo@gmail.com>
2024-03-05 14:03:54 +0200

    refactor:deleting files

--- src/components/FragmentComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function FragmentComponent(){
-    return (
-        <>
-            <h1>Topic of the hour : Fragment</h1>
-            <h2>React fragment helps to wrap multiple cjildren in one wrapper</h2>
-        </>
-    )
-}
